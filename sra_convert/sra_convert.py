0e29b7995a11bcfb67d1ab1d5eef0287169b9bfe
nsheff <nsheff@users.noreply.github.com>
2017-10-24 17:24:52 -0400

    add sra_convert

---
+++ sra_convert/sra_convert.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+import os
+import pypiper
+import sys
+
+def _parse_cmdl(cmdl):
+	parser = ArgumentParser(description="Automatic GEO SRA data downloader")
+	
+	parser.add_argument(
+			"-b", "--bamfolder", 
+			default=safe_echo("SRABAM"),
+			help="Optional: Specify a location to store bam files "
+			"[Default: $SRABAM:" + safe_echo("SRABAM") + "]")
+	
+	parser.add_argument(
+			"-s", "--srafolder", default=safe_echo("SRARAW"),
+			help="Optional: Specify a location to store sra files "
+			"[Default: $SRARAW:" + safe_echo("SRARAW") + "]")
+	
+	# parser.add_argument(
+	# 		"--picard", dest="picard_path", default=safe_echo("PICARD"),
+	# 		help="Specify a path to the picard jar, if you want to convert "
+	# 		"fastq to bam [Default: $PICARD:" + safe_echo("PICARD") + "]")
+	
+	parser.add_argument(
+			"-r", "--srr", required=True, nargs="+",
+			help="SRR accession")
+
+	parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "config"])
+	return parser.parse_args(cmdl)
+
+def safe_echo(var):
+	""" Returns an environment variable if it exists, or an empty string if not"""
+	return os.getenv(var, "")
+
+
+def main(cmdl):
+	
+	args = _parse_cmdl(cmdl)
+
+	pm = pypiper.PipelineManager(	name="sra_convert",
+									outfolder=args.srafolder,
+									args=args)
+
+	nfiles = len(args.srr)
+	for i in range(nfiles):
+		print("Processing " + str(i) + " of " + str(nfiles))
+		infile = os.path.join(args.srafolder, args.srr[i] + ".sra")
+		outfile = os.path.join(args.bamfolder, args.srr[i] + ".bam")
+		cmd = "sam-dump -u {data_source} | samtools view -bS - > {outfile}".format(
+			data_source=infile, outfile=outfile)
+
+		pm.run(cmd, target=outfile)
+
+	pm.stop_pipeline()
+
+if __name__ == "__main__":
+	main(sys.argv[1:])

e591091f5ffcbd27558d4916bf409d996f64be8e
nsheff <nsheff@users.noreply.github.com>
2022-05-11 16:43:32 -0400

    replace read_reqs

--- setup.py
+++ setup.py
@@ -11,13 +11,18 @@ REQDIR = "requirements"
 extra = {}
 
 # Ordinary dependencies
-DEPENDENCIES = []
-with open("requirements/requirements-all.txt", "r") as reqs_file:
-    for line in reqs_file:
-        if not line.strip():
-            continue
-        # DEPENDENCIES.append(line.split("=")[0].rstrip("<>"))
-        DEPENDENCIES.append(line)
+
+def read_reqs(reqs_name):
+    deps = []
+    with open(os.path.join(REQDIR, "requirements-{}.txt".format(reqs_name)), 'r') as f:
+        for l in f:
+            if not l.strip():
+                continue
+            #deps.append(l.split("=")[0].rstrip("<>"))
+            deps.append(l)
+    return deps
+
+DEPENDENCIES = read_reqs("all")
 extra["install_requires"] = DEPENDENCIES
 
 scripts = None

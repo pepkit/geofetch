728a23275edddea8cb01e4f90f52dc44d8d7b734
Khoroshevskyi <sasha99250@gmail.com>
2022-05-13 15:12:28 -0400

    joining strings corrected - corrected

--- geofetch/geofetch.py
+++ geofetch/geofetch.py
@@ -26,8 +26,8 @@ import sys
 # import tarfile
 import time
 
-from .utils import Accession, parse_accessions, parse_SOFT_line, convert_size
-from ._version import __version__
+from utils import Accession, parse_accessions, parse_SOFT_line, convert_size
+from _version import __version__
 
 from logmuse import add_logging_options, logger_via_cli
 from ubiquerg import expandpath, is_command_callable
@@ -608,7 +608,7 @@ class Geofetcher:
                         else:
                             just_string = True
                             if this_string != "":
-                                ", ".join([this_string, elem])
+                                this_string = ", ".join([this_string, elem])
                             else:
                                 this_string = elem
 

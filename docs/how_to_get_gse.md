ebe586e67a9fa1140c6692ed0bd2c9f90600f008
Khoroshevskyi <sasha99250@gmail.com>
2022-09-07 16:43:53 -0400

    Added Finder to geofetch + documentation + peppy version

--- docs/how_to_get_gse.md
+++
@@ -1,8 +0,0 @@
-# How to find and get list of files of GSEs
-
-### There is two options:
-
-1) You can create your own file with GSE numbers that you want to download using geofetch
-
-2) Use [gse_finder](https://github.com/pepkit/gse_finder) - Python package that enables get list
-of GSEs in certain period of time and at the same time use NCBI filters
--- geofetch/const.py
+++ geofetch/const.py
@@ -46,3 +46,19 @@ NEW_GENOME_COL_NAME = "ref_genome"
 
 CONFIG_PROCESSED_TEMPLATE_NAME = "config_processed_template.yaml"
 CONFIG_RAW_TEMPLATE_NAME = "config_template.yaml"
+
+# const for Finder:
+RETMAX = 10000000  # once it should be increased
+
+# gds = geo DataSets
+ETOOLS_GEO_BASE = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gds"
+ETOOLS_GEO_GSE_BASE = f"{ETOOLS_GEO_BASE}&term=GSE[ETYP]"
+
+ETOOLS_ENDING = "&retmax={retmax}&usehistory=y"
+
+TODAY_DATE = "3000"
+
+DATE_FILTER = (
+    '+AND+("{start_date}"[Publication%20Date]%20:%20"{end_date}"[Publication%20Date])'
+)
+THREE_MONTH_FILTER = '+AND+"published+last+3+months"[Filter]'
